03b2dd4069b8e0e8645d7697cc26a119efebdfb2
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-30 14:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let div=document.createElement("div")
+div.setAttribute('id','browser-info')
+div.innerHTML=`You are using+ ${navigator.appName} ${navigator.appVersion} version`
+document.body.appendChild(div)
